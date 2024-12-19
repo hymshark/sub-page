04af0124029178a5468e3461df96dac9610654bb
hyemshark <eclatant79@naver.com>
2024-12-19 12:36:20 +0900

    main.js load경로 수정

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
 $(function(){
-   $('.header').load('common/header.html'); //스크립트 작성 시 문제 발생됨. 경로부분을 없다고 인식함->경로를 common.js에 연결시켜둔다
-
-
-
+   $('.header').load('/sub-page/common/header.html'); //스크립트 작성 시 문제 발생됨. 경로부분을 없다고 인식함->경로를 common.js에 연결시켜둔다
 });//script end 
